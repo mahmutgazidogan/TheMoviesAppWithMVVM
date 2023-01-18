8c4f742789db06966f359c5e2f11c14bb1303d55
mahmutgazidogan <m.gazi.dogann>
2023-01-18 16:21:49 +0300

    most popular movies's model and service has created

---
+++ TheMoviesAppWithMVVM/TheMoviesAppWithMVVM/Constants/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  TheMoviesAppWithMVVM
+//
+//  Created by Mahmut Gazi Doğan on 18.01.2023.
+//
+
+import Foundation
---
+++ TheMoviesAppWithMVVM/TheMoviesAppWithMVVM/View/Main Screen/MostPopularMoviesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MostPopularMoviesViewController.swift
+//  TheMoviesAppWithMVVM
+//
+//  Created by Mahmut Gazi Doğan on 18.01.2023.
+//
+
+import UIKit
+
+class MostPopularMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
